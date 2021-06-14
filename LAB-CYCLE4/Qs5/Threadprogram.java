6ab638d2e9b30c8f40407f598d59b0e5780dfb30
rasikavv <rasikarajan85@gmail.com>
2021-06-14 22:21:09 +0530

    LABCYCLE4 UPLOADED

---
+++ LAB-CYCLE4/Qs5/Threadprogram.java
@@ -0,0 +1,62 @@
+package Qs5;
+import java.util.*;
+
+public class Threadprogram {
+    public static void Main(String args[]) throws InterruptedException
+    {
+        ThreadA a=new ThreadA();
+        ThreadB b=new ThreadB();
+        a.start();
+        a.sleep(200);
+        b.start();
+        b.sleep(200);
+        System.out.println("....Multithreading is over");
+
+
+    }
+    
+}
+class ThreadA extends Thread
+{
+    public void run()
+    {
+        int n=5;
+        System.out.println("Multiplication table Of 5***\n"+"\n"+"----------------------------");
+        for(int i=1;i<=10;i++)
+        {
+            
+            System.out.println("\t"+n+"X"+i+"="+n*i);
+        }
+        System.out.println("\n-------------------------------\n");
+    }
+}
+class ThreadB extends Thread
+{
+
+    public void run()
+    {
+        int i,count,j;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the limit:");
+        int l = s.nextInt();
+    System.out.println("Prime numbers between 1 and " + l + " are:");
+    for(i=1;i<=l;i++)
+    {
+        count=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+            count++;
+                
+            }
+        }
+        if(count==2)
+        {
+            System.out.println(i);
+        }
+    }
+
+    }
+    
+}
